1f711991f1e9c115ad86bd4a0236af074018fde5
maksmt <maksmtoff@gmail.com>
2022-06-23 16:06:25 +0300

    IDC-cpt/dev

--- wp-content/themes/cpt/functions.php
+++ wp-content/themes/cpt/functions.php
@@ -11,7 +11,7 @@ function cpt_scripts()
     wp_enqueue_script('jquery');
     wp_enqueue_script('slickjs', get_template_directory_uri() . '/assets/js/slick.min.js', ['jquery']);
     wp_enqueue_script('wowjs', get_template_directory_uri() . '/assets/js/wow.min.js', array('wow', null, true));
-    wp_enqueue_script('cpt-scripts', get_template_directory_uri() . '/assets/js/script.js', array(), null,true);
+    wp_enqueue_script('cpt-scripts', get_template_directory_uri() . '/assets/js/script.js', array(), null, true);
 }
 add_theme_support('custom-logo');
 add_theme_support('post-thumbnails');
